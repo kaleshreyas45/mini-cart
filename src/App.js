892f7b6e2955cb2a0559344d875f19ed86cc17e1
Shreyas Kale <kaleshreyas45@gmail.com>
2022-11-21 09:21:14 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/header';
+import Gridwall from './components/gridwall';
+import Cart from './components/cart';
+import { useSelector } from 'react-redux';
 function App() {
+  const showCart = useSelector((state)=>state.reducer2.isVisible)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      
+      {showCart?<Cart></Cart>:<Gridwall></Gridwall>}
+      
     </div>
   );
 }
